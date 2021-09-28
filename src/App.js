8a53cf96d647598c60d3b7ddb39540b5ceef1037
Abhishek <abhishe2002shukla007@gmail.com>
2021-09-28 12:40:59 +0530

    maybe first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from './components/Alert';
 
+// import About from "./components/About";
 function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) =>{
+     setAlert({
+       msg: message,
+       type: type
+     })
+     setTimeout(() => {
+       setAlert(null);    
+     }, 1500);
+  }
+
+  const toggleMode = () =>{
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'black'
+      showAlert("Dark Mode Has Been Enabled", "success")
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert("Light Mode Has Been Enabled", "success")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils2" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container">
+      <TextForm heading="Enter the Text To Analyze Below:" mode={mode} showAlert={showAlert}/>
+      {/* <About/>  */}
+      </div>
+    </>
   );
 }
 
 export default App;
+// Here, 'Navbar' is a component and 'title', 'aboutText' are props of that component.
+// 'home' prop is not defined here but still it takes its value from defaultProps in Navbar.js
---
+++ src/components/TextForm.js
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+//'useState' is used to create state variables
+
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to Upper Case!", "success");
+    }
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to Lower Case!", "success");
+    }
+    const handleClrClick = ()=>{
+        let newText = "";
+        setText(newText)
+    }
+    const handleTitClick = ()=>{
+        let newText = text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+        setText(newText)
+    }
+    const handleOnChange = (event)=>{  // this function let you to change the value of text area
+        setText(event.target.value);
+    }
+    
+
+  const [text, setText] = useState('Enter text here');
+    // 'text' is a static variable whose default value is 'Enter text here' , 'setText' is a function which lets you to change the value of 'text'  
+
+
+  return (
+    <>  
+    <div style={{color: props.mode==='dark'?'white':'black'}} >
+       <h1>{props.heading}</h1> 
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value = {text}
+          onChange = {handleOnChange} // 'handleOnChange()' will be called when value of textarea will be changed
+          id="myBox"
+          rows="8"
+          style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'black'}}
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>ConvertToUppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>ConvertToLowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleTitClick}>Title Case</button>
+      <button className="btn btn-primary mx-2" onClick={handleClrClick}>Clear</button>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes will required to read</p>
+        <h5>{text}</h5>
+    </div>
+    </>
+  );
+}
